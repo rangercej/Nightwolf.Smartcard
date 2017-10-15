c94dc11a6f818a67ea6617b63165c656d5ced27f
Chris J <cej@nightwolf.org.uk>
2017-10-15 13:57:56 +0100

    Move SC events into new thread; marshal comms between thread and form to allow blocking reads from event queue without hanging GUI thread

---
+++ SmartTrigger/CardMonitor.cs
@@ -0,0 +1,178 @@
+﻿namespace Nightwolf.SmartTrigger
+{
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading;
+    using System.Windows.Forms;
+
+    using Nightwolf.Smartcard;
+
+    internal class CardMonitor
+    {
+        /// <summary>Smartcard monitor class</summary>
+        private readonly SmartcardMonitor smartcardMonitor;
+
+        /// <summary>Cache of certificate subjects associated with a reader</summary>
+        private readonly Dictionary<string, List<string>> certificateCache;
+
+        /// <summary>Smartcard event queue</summary>
+        private readonly BlockingCollection<EventParams> eventQueue = new BlockingCollection<EventParams>(new ConcurrentQueue<EventParams>());
+
+        /// <summary>app.config configuration</summary>
+        private readonly Config.Smartcard configuration;
+
+        private CancellationToken cancellationToken;
+
+        private ScPinWindow RequestPinForm;
+
+        internal CardMonitor(CancellationToken ct)
+        {
+            this.configuration = (Config.Smartcard)ConfigurationManager.GetSection("smartcard");
+
+            this.cancellationToken = ct;
+
+            this.smartcardMonitor = new SmartcardMonitor();
+            this.smartcardMonitor.OnCardInserted += this.CardInsertedEventHandler;
+            this.smartcardMonitor.OnCardRemoved += this.CardRemovedEventHandler;
+
+            this.certificateCache = new Dictionary<string, List<string>>();
+            this.MonitoringStopped = new ManualResetEvent(false);
+        }
+
+        protected enum EventType
+        {
+            Insert,
+            Remove
+        }
+
+        internal ManualResetEvent MonitoringStopped { get; }
+
+        internal void Start(ScPinWindow pinForm)
+        {
+            this.RequestPinForm = pinForm;
+            this.smartcardMonitor.StartMonitoring(this.cancellationToken);
+            while (!this.cancellationToken.IsCancellationRequested)
+            {
+                var evt = this.eventQueue.Take(this.cancellationToken);
+                if (evt.Action == EventType.Insert)
+                {
+                    this.DoCardInserted(evt.EventArgs);
+                }
+                else if (evt.Action == EventType.Remove)
+                {
+                    this.DoCardRemoved(evt.EventArgs);
+                }
+            }
+
+            this.smartcardMonitor.MonitoringStoppedTrigger.WaitOne();
+            this.MonitoringStopped.Set();
+        }
+
+        /// <summary>
+        /// Smartcard inserted handler
+        /// </summary>
+        /// <param name="e">Smartcard event args</param>
+        private void DoCardInserted(SmartcardEventArgs e)
+        {
+            var processor = new ActionProcessor();
+
+            var certList = new List<string>();
+
+            // Find matching actions
+            foreach (var cardCert in e.SmartCard.CertificateStore.Certificates)
+            {
+                var subj = cardCert.Subject;
+                var certActions = this.configuration.Certificates.Where(x => x.Subject == subj).ToList();
+                certList.Add(subj);
+
+                foreach (var action in certActions)
+                {
+                    var insertActions = action.Actions.Where(act => (act.OnEvent & Config.Action.SmartcardAction.Insert) == Config.Action.SmartcardAction.Insert);
+                    processor.AddActions(e.SmartCard, action.Subject, insertActions);
+                }
+            }
+
+            this.certificateCache.Add(e.ReaderName, certList);
+
+            // Any actions allocated to this smartcard?
+            if (processor.ActionCount == 0)
+            {
+                return;
+            }
+
+            // Do any actions want the PIN?
+            if (processor.PinRequired)
+            {
+                var mre = new ManualResetEvent(false);
+                this.RequestPinForm.Invoke((MethodInvoker)(() => this.RequestPinForm.ShowPinWindow(mre)));
+                mre.WaitOne();
+                processor.ProcessInsertActions(this.RequestPinForm.PinPassback);
+            }
+            else
+            {
+                processor.ProcessInsertActions(null);
+            }
+        }
+
+        /// <summary>
+        /// Smartcard removed handler
+        /// </summary>
+        /// <param name="e">Smartcard event args</param>
+        private void DoCardRemoved(SmartcardEventArgs e)
+        {
+            var processor = new ActionProcessor();
+
+            var certs = this.certificateCache[e.ReaderName];
+
+            // Find matching actions
+            foreach (var subj in certs)
+            {
+                var certActions = this.configuration.Certificates.Where(x => x.Subject == subj).ToList();
+
+                foreach (var action in certActions)
+                {
+                    var removeActions = action.Actions.Where(act => (act.OnEvent & Config.Action.SmartcardAction.Remove) == Config.Action.SmartcardAction.Remove);
+                    processor.AddActions(e.SmartCard, action.Subject, removeActions);
+                }
+            }
+
+            // Any actions allocated to this smartcard?
+            if (processor.ActionCount == 0)
+            {
+                return;
+            }
+
+            processor.ProcessRemoveActions();
+            this.certificateCache.Remove(e.ReaderName);
+        }
+
+        /// <summary>
+        /// Receive smartcard insert events and marshal them onto the forms thread
+        /// </summary>
+        /// <param name="o">Sender of the event</param>
+        /// <param name="e">Smartcard event arguments</param>
+        private void CardInsertedEventHandler(object o, SmartcardEventArgs e)
+        {
+            this.eventQueue.Add(new EventParams {Action = EventType.Insert, EventArgs = e});
+        }
+
+        /// <summary>
+        /// Receive smartcard removed events and marshal them onto the forms thread
+        /// </summary>
+        /// <param name="o">Sender of the event</param>
+        /// <param name="e">Smartcard event arguments</param>
+        private void CardRemovedEventHandler(object o, SmartcardEventArgs e)
+        {
+            this.eventQueue.Add(new EventParams { Action = EventType.Remove, EventArgs = e });
+        }
+
+        protected struct EventParams
+        {
+            internal EventType Action;
+
+            internal SmartcardEventArgs EventArgs;
+        }
+    }
+}
--- SmartTrigger/Form1.cs
+++ SmartTrigger/Form1.cs
@@ -5,10 +5,9 @@ using System.Collections.Generic;
 namespace Nightwolf.SmartTrigger
 {
     using System;
-    using System.Configuration;
     using System.Drawing;
-    using System.Linq;
     using System.Threading;
+    using System.Threading.Tasks;
     using System.Windows.Forms;
 
     using Nightwolf.Smartcard;
@@ -19,23 +18,15 @@ namespace Nightwolf.SmartTrigger
     /// <inheritdoc cref="Form"/>
     public partial class ScPinWindow : Form
     {
-        /// <summary>Smartcard monitor class</summary>
-        private readonly SmartcardMonitor smartcardMonitor;
+        private ManualResetEvent monitorNotify;
 
-        /// <summary>Cache of certificate subjects associated with a reader</summary>
-        private readonly Dictionary<string, List<string>> certificateCache;
-
-        /// <summary>app.config configuration</summary>
-        private readonly Config.Smartcard configuration;
+        internal string PinPassback;
 
         /// <summary>Notification tray icon</summary>
         private NotifyIcon trayIcon;
 
         /// <summary>Smartcard monitor cancellation</summary>
         private CancellationTokenSource monitorCancellationToken;
-
-        /// <summary>Actions to process</summary>
-        private ActionProcessor processor;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ScPinWindow"/> class. 
@@ -44,14 +35,6 @@ namespace Nightwolf.SmartTrigger
         public ScPinWindow()
         {
             this.InitializeComponent();
-
-            this.configuration = (Config.Smartcard)ConfigurationManager.GetSection("smartcard");
-
-            this.smartcardMonitor = new SmartcardMonitor();
-            this.smartcardMonitor.OnCardInserted += this.CardInsertedEventHandler;
-            this.smartcardMonitor.OnCardRemoved += this.CardRemovedEventHandler;
-
-            this.certificateCache = new Dictionary<string, List<string>>();
         }
 
         /// <summary>
@@ -72,102 +55,9 @@ namespace Nightwolf.SmartTrigger
             this.ShowInTaskbar = false;
 
             base.OnLoad(e);
-
             this.monitorCancellationToken = new CancellationTokenSource();
-            this.smartcardMonitor.StartMonitoring(this.monitorCancellationToken.Token);
-        }
-
-        /// <summary>
-        /// Smartcard inserted handler
-        /// </summary>
-        /// <param name="e">Smartcard event args</param>
-        private void DoCardInserted(SmartcardEventArgs e)
-        {
-            // If there's an active processor, wait for it to complete processing
-            this.processor?.Wait();
-
-            this.processor = new ActionProcessor();
-
-            var certList = new List<string>();
-
-            // Find matching actions
-            foreach (var cardCert in e.SmartCard.CertificateStore.Certificates)
-            {
-                var subj = cardCert.Subject;
-                var certActions = this.configuration.Certificates.Where(x => x.Subject == subj).ToList();
-                certList.Add(subj);
-
-                foreach (var action in certActions)
-                {
-                    var insertActions = action.Actions.Where(act => (act.OnEvent & Config.Action.SmartcardAction.Insert) == Config.Action.SmartcardAction.Insert);
-                    this.processor.AddActions(e.SmartCard, action.Subject, insertActions);
-                }
-            }
-
-            this.certificateCache.Add(e.ReaderName, certList);
-
-            // Any actions allocated to this smartcard?
-            if (this.processor.ActionCount == 0)
-            {
-                this.processor.Reset();
-                return;
-            }
-
-            // Do any actions want the PIN?
-            if (this.processor.PinRequired)
-            {
-                this.Visible = true;
-                this.ShowInTaskbar = true;
-                this.Focus();
-            }
-            else
-            {
-                this.processor.ProcessInsertActions(null);
-            }
-        }
-
-        /// <summary>
-        /// Smartcard removed handler
-        /// </summary>
-        /// <param name="e">Smartcard event args</param>
-        private void DoCardRemoved(SmartcardEventArgs e)
-        {
-            if (this.Visible)
-            {
-                this.Visible = false;
-                this.ShowInTaskbar = false;
-            }
-
-            // If there's an active processor, wait for it to complete processing
-            this.processor?.Wait();
-
-            this.processor = new ActionProcessor();
-
-            var certs = this.certificateCache[e.ReaderName];
-
-            // Find matching actions
-            foreach (var subj in certs)
-            {
-                var certActions = this.configuration.Certificates.Where(x => x.Subject == subj).ToList();
-
-                foreach (var action in certActions)
-                {
-                    var removeActions = action.Actions.Where(act => (act.OnEvent & Config.Action.SmartcardAction.Remove) == Config.Action.SmartcardAction.Remove);
-                    this.processor.AddActions(e.SmartCard, action.Subject, removeActions);
-                }
-            }
-
-            // Any actions allocated to this smartcard?
-            if (this.processor.ActionCount == 0)
-            {
-                this.processor.Reset();
-            }
-            else
-            {
-                this.processor.ProcessRemoveActions();
-            }
-
-            this.certificateCache.Remove(e.ReaderName);
+            var monitor = new CardMonitor(this.monitorCancellationToken.Token);
+            Task.Factory.StartNew(() => monitor.Start(this));
         }
 
         /// <summary>
@@ -184,6 +74,14 @@ namespace Nightwolf.SmartTrigger
             }
         }
 
+        internal void ShowPinWindow(ManualResetEvent continueNotifier)
+        {
+            this.monitorNotify = continueNotifier;
+            this.Visible = true;
+            this.ShowInTaskbar = true;
+            this.Focus();
+        }
+
         /// <summary>
         /// When form is closed, cleanly shutdown the smartcard monitor
         /// </summary>
@@ -191,44 +89,16 @@ namespace Nightwolf.SmartTrigger
         /// <param name="e">Form close parameters</param>
         private void ScPinWindow_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (this.smartcardMonitor.IsMonitoring)
-            {
-                this.monitorCancellationToken.Cancel();
-                this.smartcardMonitor.MonitoringStoppedTrigger.WaitOne();
-            }
-
+            this.monitorCancellationToken.Cancel();
             this.trayIcon.Dispose();
         }
-
-        #region Smartcard Event Handlers
-
-        /// <summary>
-        /// Receive smartcard insert events and marshal them onto the forms thread
-        /// </summary>
-        /// <param name="o">Sender of the event</param>
-        /// <param name="e">Smartcard event arguments</param>
-        private void CardInsertedEventHandler(object o, SmartcardEventArgs e)
-        {
-            this.Invoke((MethodInvoker)(() => this.DoCardInserted(e)));
-        }
-
-        /// <summary>
-        /// Receive smartcard removed events and marshal them onto the forms thread
-        /// </summary>
-        /// <param name="o">Sender of the event</param>
-        /// <param name="e">Smartcard event arguments</param>
-        private void CardRemovedEventHandler(object o, SmartcardEventArgs e)
-        {
-            this.Invoke((MethodInvoker)(() => this.DoCardRemoved(e)));
-        }
-
-        #endregion
 
         private void btnUnlock_Click(object sender, EventArgs e)
         {
             this.Visible = false;
             this.ShowInTaskbar = false;
-            this.processor.ProcessInsertActions(this.textPin.Text);
+            this.PinPassback = this.textPin.Text;
+            this.monitorNotify.Set();
         }
     }
 }
